fb184aae1d262d0e107e1816ed3f54e6e4cd5070
Ivan <ki16082005@gmail.com>
2024-05-30 22:56:11 +0300

    Добавлены страницы для работы с таблицами, изменена навигация и оформлена главная страница

---
+++ The_All_Breeds_Dogs_Club/Pages/Request.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace The_All_Breeds_Dogs_Club.Pages
+{
+    public class RequestModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
